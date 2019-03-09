d6db7f9ce5b6d0858d71d174b8cb5b223154a927
aditya10185 <aditya10185@csu.fullerton.edu>
2019-03-09 13:49:30 -0800

    server

---
+++ mroomserver/src/main/java/com/meetingroom/mroomserver/MroomserverApplication.java
@@ -0,0 +1,14 @@
+package com.meetingroom.mroomserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MroomserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MroomserverApplication.class, args);
+	}
+
+}
+
